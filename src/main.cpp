7bed483f21ad4c4555e6cd251729759d857e5877
trkind <tgkindvall@gmail.com>
2020-02-11 20:13:08 -0700

    added global light values

--- src/main.cpp
+++ src/main.cpp
@@ -1,4 +1,8 @@
 #include <Arduino.h>
+#include <Adafruit_NeoPixel.h>
+
+#define LED_PIN = 5 
+#define LED_NUM = 5 /* this is a placeholder */ 
 
 void setup() {
   // put your setup code here, to run once:
