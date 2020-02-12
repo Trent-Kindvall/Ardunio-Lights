fc962b19a8b2bcdafa4ed9cd75481ec14a20f3f8
trkind <tgkindvall@gmail.com>
2020-02-11 20:25:46 -0700

    add first light test

--- src/main.cpp
+++ src/main.cpp
@@ -4,10 +4,22 @@
 #define LED_PIN = 5 
 #define LED_NUM = 5 /* this is a placeholder */ 
 
+Adafruit_NeoPixel pixels = Adafruit_NeoPixel(LED_NUM, LED_PIN, NEO_GRB + NEO_KHZ800);
+int delayval = 500;
+
 void setup() {
-  // put your setup code here, to run once:
+  pixels.begin();
+
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+
+  for(int i=0;i< LED_NUM;i++){
+
+    pixels.setPixelColor(i,pixels.Color(100,100,100));
+    pixels.show()
+    delay(delayval);
+
+  }
+
 }
