e09cb5bd0391ba6c6a5cac7f72b12b24ffecd5e5
trkind <tgkindvall@gmail.com>
2020-02-16 15:54:09 -0700

    add wifi modules

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,11 @@
 #include <Arduino.h>
-#include <Adafruit_NeoPixel.h>
+#include <Adafruit_NeoPixel.h> // Lights
 
-#define LED_PIN 14 /* turns out pin 5 was a good guess */
+#include <DNSServer.h>            //Local DNS Server used for redirecting all requests to the configuration portal
+#include <ESP8266WebServer.h>     //Local WebServer used to serve the configuration portal
+#include <WiFiManager.h>          //https://github.com/tzapu/WiFiManager WiFi Configuration Magic
+
+#define LED_PIN 14 // this is actualy pin 5 but is graphed to pin 14
 #define LED_NUM 192
 
 Adafruit_NeoPixel pixels = Adafruit_NeoPixel(LED_NUM, LED_PIN, NEO_GRB + NEO_KHZ800);
@@ -11,6 +15,9 @@ void setup()
 {
   pixels.begin();
   pixels.show();
+  WiFiManager wifiManager;
+  wifiManager.autoConnect("TNT","Pleaseme1");
+
 }
 
 void loop()
